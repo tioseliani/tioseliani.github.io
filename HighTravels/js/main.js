a62e5458393ddb27a033f6d3123b9fe394354260
Tako <tako.ioseliani06b@geolab.edu.ge>
2019-09-16 20:18:13 +0400

    Add files via upload

---
+++ HighTravels/js/main.js
@@ -0,0 +1,9 @@
+// Header Slider
+$(document).ready(function(){
+    $('.header-slider').slick({
+      Type: Boolean,
+      Default: false,
+      arrows: false,
+      autoplay: true      
+    });
+});
